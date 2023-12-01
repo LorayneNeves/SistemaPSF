84804d6518221beb10e4c0fc53f0f61c0b89a490
LorayneNeves <lorayne3.8neves@outlook.com>
2023-11-30 22:15:08 -0300

    update style css

---
+++ 03 - Desenvolvimento/SistemaPSF/PSF/src/PSF.WebApp/Models/EventoIndicadorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PSF.WebApp.Models
+{
+    public class EventoIndicadorViewModel
+    {
+        public int EventoId { get; set; }
+        public string TituloIndicador { get; set; }
+        public int Valor { get; set; }
+    }
+}
---
+++ 03 - Desenvolvimento/SistemaPSF/PSF/src/PSF.WebApp/Models/EventoViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace PSF.WebApp.Models
+{
+    public class EventoViewModel
+    {
+        public List<EventoIndicadorViewModel> EventosIndicadores { get; set; }
+    }
+}
